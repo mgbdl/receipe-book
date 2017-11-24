0017459f6530ec196e0bf3dd6fc39fdeed4ee8b1
Miguel Breceda <miguel.breceda@outlook.es>
2017-11-24 16:44:28 -0600

    create: shopping-list & shopping-list-ad comp, update: module , html app.comp

---
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-add',
+  templateUrl: './shopping-list-add.component.html',
+  // styleUrls: ['./shopping-list-add.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ShoppingListAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
